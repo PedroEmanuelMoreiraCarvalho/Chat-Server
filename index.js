c507d7868bb4e00c70046ba6fd9c247b4921f721
PedroEmanuelMoreiraCarvalho <91740410+PedroEmanuelMoreiraCarvalho@users.noreply.github.com>
2022-03-22 14:38:14 -0300

    server files

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const app = require('express')();
+const server = require('http').createServer(app);
+const port  = process.env.PORT || 8000;
+const io = require('socket.io')(server,{
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }}
+);
+
+const messages = []
+
+io.on('connection', (socket) => {
+  console.log("usuario conectado",socket.id);
+  socket.on('message', (data) => {
+    messages.push(data)
+    io.emit('updateMessages', data)
+  });
+});
+server.listen(port);
