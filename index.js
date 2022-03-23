21c0c7b54582e77b2a68189c5eb2628091873f5e
PedroEmanuelMoreiraCarvalho <91740410+PedroEmanuelMoreiraCarvalho@users.noreply.github.com>
2022-03-23 16:51:25 -0300

    increased performance

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 
 const app = require('express')();
 const server = require('http').createServer(app);
+const ws = require('ws')
+const parser = require("socket.io-msgpack-parser");
 const port  = process.env.PORT || 8000;
 const io = require('socket.io')(server,{
   cors: {
     origin: "*",
     methods: ["GET", "POST"]
-  }}
-);
-
-app.get("/",(req,res)=>{
-  res.send("Server Chat Workind!!")
+  },
+  wsEngine: ws.Server,
+  parser
 });
+
+var messages = []
+
+setInterval(()=>{
+  messages = []
+},1800000)
 
 io.on('connection', (socket) => {
   console.log("usuario conectado",socket.id);
-  socket.on('message', (data) => {
-    io.emit('updateMessages', data)
+  socket.on("message", (data) => {
+    io.emit('updateMessages', messages)
   });
 });
+
 
 server.listen(port);
