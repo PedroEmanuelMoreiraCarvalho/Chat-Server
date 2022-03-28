15cb5e123c1e7f0a84d52c1389a1397976e1323a
PedroEmanuelMoreiraCarvalho <91740410+PedroEmanuelMoreiraCarvalho@users.noreply.github.com>
2022-03-28 17:07:26 -0300

    new funcitions to WatchTogether project
    playlist, skip and restart

--- index.js
+++ index.js
@@ -14,12 +14,17 @@ const io = require('socket.io')(server,{
 });
 
 var connections = []
+var playlist = []
+var next_requests = 0
+var restart_requests = 0
 var users_online = -1
 
 io.on('connection', (socket) => {
   connections.push(socket)
   users_online++
   socket.messages = []
+  socket.next_req = false
+  socket.restart_req = false
   socket.user_name = "Alguém"
   connections.forEach((_socket)=>{
     if(_socket!=socket)
@@ -30,6 +35,8 @@ io.on('connection', (socket) => {
     if(_socket!=socket)
     io.to(_socket.id).emit("updateMessages", _socket.messages)
   })
+  io.to(socket.id).emit("updatePlaylist", playlist)
+  io.to(socket.id).emit("updateVideo",{video: playlist[0]})
 
   socket.on("message", (data) => {
     socket.user_name = data.user
@@ -40,6 +47,105 @@ io.on('connection', (socket) => {
       io.to(socket.id).emit("updateMessages", socket.messages)
     })
   });
+
+  socket.on("pauseHandled",() => {
+    connections.forEach((_socket)=>{
+      _socket.messages.push({author: 1, user: "server", message: `${socket.user_name} pausou o vídeo`})
+      io.to(_socket.id).emit("updateMessages", _socket.messages)
+    })
+  })
+
+  socket.on("playHandled",() => {
+    connections.forEach((_socket)=>{
+      _socket.messages.push({author: 1, user: "server", message: `${socket.user_name} retomou o vídeo`})
+      io.to(_socket.id).emit("updateMessages", _socket.messages)
+    })
+  })
+
+  socket.on("videoEnded",() => {
+    next_requests=0
+    connections.forEach((_socket)=>{
+      _socket.next_req = false
+    })
+
+    playlist.shift()
+    connections.forEach((_socket)=>{
+      io.to(_socket.id).emit("updateVideo",{video: playlist[0]})
+    })
+    connections.forEach((socket)=>{
+      io.to(socket.id).emit("updatePlaylist", playlist)
+    })
+  })
+
+  socket.on("addvideo",(video) => {
+    playlist.push(video.video)
+    connections.forEach((socket)=>{
+      io.to(socket.id).emit("updatePlaylist", playlist)
+    })
+    if(playlist[1]==undefined){
+      connections.forEach((_socket)=>{
+        io.to(_socket.id).emit("updateVideo",{video: playlist[0]})
+      })
+    }
+  })
+
+  socket.on("next_request",()=>{
+    let req_needed_to_skip = Math.floor((users_online+1)/2)
+    if(!socket.next_req){
+      next_requests++
+      socket.next_req = true
+      connections.forEach((_socket)=>{
+        _socket.messages.push({author: 1, user: "server", message: `${socket.user_name} pediu para skipar o vídeo (${next_requests}/${req_needed_to_skip})`})
+        io.to(_socket.id).emit("updateMessages", _socket.messages)
+      })
+    }
+    if(next_requests>=req_needed_to_skip){
+      next_requests=0
+      connections.forEach((_socket)=>{
+        _socket.next_req = false
+      })
+
+      playlist.shift()
+      connections.forEach((_socket)=>{
+        io.to(_socket.id).emit("updateVideo",{video: playlist[0]})
+      })
+      connections.forEach((socket)=>{
+        io.to(socket.id).emit("updatePlaylist", playlist)
+      })
+      connections.forEach((socket)=>{
+        socket.messages.push({author: 1, user: "server", message: `vídeo skipado`})
+        io.to(socket.id).emit("updateMessages", socket.messages)
+      })
+    }
+  })
+
+  socket.on("restart",()=>{
+    let req_needed_to_restart = Math.floor((users_online+1)/2)
+    if(!socket.restart_req){
+      restart_requests++
+      socket.restart_req = true
+      connections.forEach((_socket)=>{
+        _socket.messages.push({author: 1, user: "server", message: `${socket.user_name} pediu para reiniciar o vídeo (${restart_requests}/${req_needed_to_restart})`})
+        io.to(_socket.id).emit("updateMessages", _socket.messages)
+      })
+    }
+    if(restart_requests>=req_needed_to_restart){
+      restart_requests=0
+      connections.forEach((_socket)=>{
+        _socket.restart_req = false
+      })
+
+      connections.forEach((_socket)=>{
+        io.to(_socket.id).emit("updateVideo",{video: undefined})
+        io.to(_socket.id).emit("updateVideo",{video: playlist[0]})
+      })
+      
+      connections.forEach((socket)=>{
+        socket.messages.push({author: 1, user: "server", message: `vídeo reiniciado`})
+        io.to(socket.id).emit("updateMessages", socket.messages)
+      })
+    }
+  })
 
   socket.on("typing",()=>{
     usertyping = socket.user_name
